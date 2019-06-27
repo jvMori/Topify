a25e61fa895860ef1c84d6acdc984e1e81efc1cc
Mori <iwona.moryc92@gmail.com>
2019-06-27 10:54:11 +0200

    di network module

---
+++ app/src/main/java/com/jvmori/topify/di/module/ApiModule.kt
@@ -0,0 +1,37 @@
+package com.jvmori.topify.di.module
+
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import com.jvmori.topify.data.SpotifyApi
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import com.jvmori.topify.di.scope.ApplicationScope
+import dagger.Module
+import dagger.Provides
+
+
+@Module(includes = [NetworkModule::class])
+class ApiModule {
+
+    companion object {
+        private const val BASE_URL = "https://api.spotify.com/v1"
+    }
+
+    @Provides
+    @ApplicationScope
+    fun spotifyApi(retrofit: Retrofit): SpotifyApi {
+        return retrofit.create<SpotifyApi>(SpotifyApi::class.java)
+    }
+
+    @Provides
+    @ApplicationScope
+    fun retrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+}
---
+++ app/src/main/java/com/jvmori/topify/di/module/NetworkModule.kt
@@ -0,0 +1,49 @@
+package com.jvmori.topify.di.module
+
+import okhttp3.OkHttpClient
+import com.jvmori.topify.di.scope.ApplicationScope
+import dagger.Module
+import dagger.Provides
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import javax.inject.Named
+
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @ApplicationScope
+    @Named("Access_Token")
+    fun provideAccessToken() : String {
+        return ""
+    }
+
+    @Provides
+    @ApplicationScope
+    fun provideInterceptor(@Named("Access_Token") accessToken : String): Interceptor {
+        return Interceptor { chain ->
+            val url = chain.request()
+                .url()
+                .newBuilder()
+                .build()
+
+            val request = chain.request()
+                .newBuilder()
+                .addHeader("Authorization", "Bearer $accessToken")
+                .url(url)
+                .build()
+            return@Interceptor chain.proceed(request)
+        }
+    }
+
+
+    @Provides
+    @ApplicationScope
+    fun okHttpClient(interceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+}
