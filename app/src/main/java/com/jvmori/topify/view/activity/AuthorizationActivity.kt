ec0e97eb1a5b77ad2f73e8b32f6e5637fa6fd2db
Mori <iwona.moryc92@gmail.com>
2019-06-27 12:22:50 +0200

    authorization

--- app/src/main/java/com/jvmori/topify/view/activity/AuthorizationActivity.kt
+++ app/src/main/java/com/jvmori/topify/view/activity/AuthorizationActivity.kt
@@ -1,16 +1,30 @@
 package com.jvmori.topify.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.ViewModelProviders
 import com.jvmori.topify.R
+import com.jvmori.topify.view.viewmodel.AuthViewModel
 
 
 class AuthorizationActivity : AppCompatActivity() {
 
+    private lateinit var authViewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("TOPIFY", "MAIN ACTIVITY")
+
+        authViewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
+        authViewModel.authorize(this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let {
+            authViewModel.checkResponse(it)
+        }
     }
 }
---
+++ app/src/main/java/com/jvmori/topify/view/viewmodel/AuthViewModel.kt
@@ -0,0 +1,40 @@
+package com.jvmori.topify.view.viewmodel
+
+import android.app.Activity
+import android.content.Intent
+import androidx.lifecycle.ViewModel
+import com.jvmori.topify.Utils.CLIENT_ID
+import com.jvmori.topify.data.network.AccessToken
+import com.spotify.sdk.android.authentication.AuthenticationClient
+import com.spotify.sdk.android.authentication.AuthenticationRequest
+import com.spotify.sdk.android.authentication.AuthenticationResponse
+
+class AuthViewModel : ViewModel() {
+    private val REDIRECT_URI = "https://topify"
+
+    fun authorize(activity: Activity) {
+        val builder = AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI)
+        builder.setScopes(arrayOf("user-read-private", "user-read-email"))
+        val request = builder.build()
+        AuthenticationClient.openLoginInBrowser(activity, request)
+    }
+
+    fun checkResponse(intent: Intent) {
+        val uri = intent.data
+        if (uri != null) {
+            val response = AuthenticationResponse.fromUri(uri)
+            when (response.type) {
+                AuthenticationResponse.Type.TOKEN ->
+                {
+                    AccessToken.accessToken = response.accessToken
+                }
+
+                AuthenticationResponse.Type.ERROR ->
+                {
+
+                }
+                else -> {}
+            }
+        }
+    }
+}
