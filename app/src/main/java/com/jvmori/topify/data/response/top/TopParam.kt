dc7029403e696cd341bfc929c27c020f364795fc
Mori <iwona.moryc92@gmail.com>
2019-07-03 16:47:51 +0200

    displaying top tracks in view

--- app/src/main/java/com/jvmori/topify/data/response/top/TopParam.kt
+++ app/src/main/java/com/jvmori/topify/data/response/top/TopParam.kt
@@ -1,6 +1,11 @@
 package com.jvmori.topify.data.response.top
 
 data class TopParam (
-    val limit : Int,
-    val timeRange : String
+    val limit : Int = 50,
+    val timeRange : String = TimeRange().mediumTerm
+)
+data class TimeRange(
+    val longTerm : String = "long_term",
+    val mediumTerm : String = "medium_term",
+    val shortTerm : String = "short_term"
 )
--- app/src/main/java/com/jvmori/topify/view/fragment/FragmentCreateTop.kt
+++ app/src/main/java/com/jvmori/topify/view/fragment/FragmentCreateTop.kt
@@ -2,13 +2,23 @@ package com.jvmori.topify.view.fragment
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 import com.jvmori.topify.R
+import com.jvmori.topify.data.Resource
+import com.jvmori.topify.data.response.top.TimeRange
+import com.jvmori.topify.data.response.top.TopParam
+import com.jvmori.topify.data.response.top.TopTracksResponse
+import com.jvmori.topify.view.viewmodel.CreateTopViewModel
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +31,9 @@ private const val ARG_PARAM2 = "param2"
  */
 class FragmentCreateTop : DaggerFragment() {
 
+    @Inject
+    lateinit var factory : ViewModelProvider.Factory
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,5 +42,29 @@ class FragmentCreateTop : DaggerFragment() {
         return inflater.inflate(R.layout.fragment_create_top, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val topViewModel = ViewModelProviders.of(this, factory)[CreateTopViewModel::class.java]
+        val params = TopParam(50, TimeRange().shortTerm) //TODO: user can change it in settings
+        topViewModel.fetchTopTracks(params)
+        topViewModel.topTracks().observe(this, Observer {
+            when (it.status) {
+                Resource.Status.LOADING -> showLoading()
+                Resource.Status.SUCCESS -> success(it.data)
+                Resource.Status.ERROR -> error(it.message)
+            }
+        })
+    }
+    
+    private fun showLoading(){
 
+    }
+
+    private fun success(data : TopTracksResponse?){
+        Log.i("TOPIFY", data?.tracks.toString() )
+    }
+
+    private fun error(message : String?){
+        Log.i("TOPIFY", message )
+    }
 }
