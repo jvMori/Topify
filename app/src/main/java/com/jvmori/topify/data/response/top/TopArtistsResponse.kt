a491e3a41afadc452e4851ae701d0f151503b823
jvMori <iwona.moryc92@gmail.com>
2019-07-03 20:08:40 +0200

    null ref error fixed on tracks

--- app/src/main/java/com/jvmori/topify/data/response/top/TopArtistsResponse.kt
+++ app/src/main/java/com/jvmori/topify/data/response/top/TopArtistsResponse.kt
@@ -1,7 +1,10 @@
 package com.jvmori.topify.data.response.top
 
+import com.google.gson.annotations.SerializedName
+
 
 data class TopArtistsResponse(
     val href: String,
+    @SerializedName("items")
     val artists: List<ArtistItem>
 )
--- app/src/main/java/com/jvmori/topify/data/response/top/TopTracksResponse.kt
+++ app/src/main/java/com/jvmori/topify/data/response/top/TopTracksResponse.kt
@@ -1,7 +1,10 @@
 package com.jvmori.topify.data.response.top
 
+import com.google.gson.annotations.SerializedName
+
 
 data class TopTracksResponse(
     val href: String,
+    @SerializedName("items")
     val tracks: List<Track>
 )
