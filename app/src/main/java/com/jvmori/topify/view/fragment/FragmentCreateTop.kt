feca18a2a71c954bf3cd708e47a304e42677d40c
jvMori <iwona.moryc92@gmail.com>
2019-09-19 19:37:42 +0200

    refactoring -> fetch user id for playlist creation dynamically

--- app/src/main/java/com/jvmori/topify/view/fragment/FragmentCreateTop.kt
+++ app/src/main/java/com/jvmori/topify/view/fragment/FragmentCreateTop.kt
@@ -3,25 +3,25 @@ package com.jvmori.topify.view.fragment
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
 import com.jvmori.topify.R
 import com.jvmori.topify.Utils.ImageLoader
+import com.jvmori.topify.Utils.SessionManager
 import com.jvmori.topify.data.Resource
 import com.jvmori.topify.data.response.playlist.AddTracks
-import com.jvmori.topify.data.response.playlist.PlaylistResponse
 import com.jvmori.topify.data.response.top.TimeRange
 import com.jvmori.topify.data.response.top.TopParam
 import com.jvmori.topify.data.response.top.TopTracksResponse
 import com.jvmori.topify.data.response.top.Track
+import com.jvmori.topify.view.activity.AuthResource
 import com.jvmori.topify.view.adapters.TopTracksAdapter
 import com.jvmori.topify.view.viewmodel.CreateTopViewModel
 import dagger.android.support.DaggerFragment
@@ -40,10 +40,13 @@ private const val ARG_PARAM2 = "param2"
 class FragmentCreateTop : DaggerFragment() {
 
     @Inject
-    lateinit var factory : ViewModelProvider.Factory
+    lateinit var factory: ViewModelProvider.Factory
 
     @Inject
     lateinit var imageLoader: ImageLoader
+
+    @Inject
+    lateinit var sessionManager: SessionManager
 
     private lateinit var topViewModel: CreateTopViewModel
     private val tracksUris = mutableListOf<String>()
@@ -58,7 +61,7 @@ class FragmentCreateTop : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-       // activity?.setActionBar(my_toolbar)
+        // activity?.setActionBar(my_toolbar)
         topViewModel = ViewModelProviders.of(this, factory).get(CreateTopViewModel::class.java)
         displayTop()
         createPlaylist()
@@ -68,14 +71,19 @@ class FragmentCreateTop : DaggerFragment() {
     }
 
     private fun createPlaylist() {
-        create_btn.setOnClickListener {
-            topViewModel.createTopTracksPlaylist("jeivsss", "Topify top 50")
-        }
+        sessionManager.getUser().observe(this, Observer { user ->
+            when (user.status) {
+                AuthResource.AuthStatus.AUTHENTICATED-> {
+                    create_btn.setOnClickListener {
+                        topViewModel.createTopTracksPlaylist(user.data?.id!!, "Topify top 50")
+                    }
+                }
+            }
+        })
         topViewModel.topTracksPlaylist().observe(this, Observer {
             when (it.status) {
                 Resource.Status.LOADING -> showLoading()
-                Resource.Status.SUCCESS ->{
-                    Log.i("TOPIFY", it.data?.name.toString())
+                Resource.Status.SUCCESS -> {
                     topViewModel.addTracksToPlaylist(it.data?.id!!, AddTracks(uris = tracksUris))
                 }
                 Resource.Status.ERROR -> error(it.message)
@@ -97,26 +105,26 @@ class FragmentCreateTop : DaggerFragment() {
         })
     }
 
-    private fun showLoading(){
+    private fun showLoading() {
 
     }
 
-    private fun success(data : TopTracksResponse?){
+    private fun success(data: TopTracksResponse?) {
         createTopTracksAdapter(data?.tracks)
         createUris(data)
     }
 
-    private fun createUris(data : TopTracksResponse?){
+    private fun createUris(data: TopTracksResponse?) {
         data?.tracks?.forEach { item ->
             tracksUris.add(item.uri)
         }
     }
 
-    private fun error(message : String?){
-        Log.i("TOPIFY", message )
+    private fun error(message: String?) {
+        Log.i("TOPIFY", message)
     }
 
-    private fun createTopTracksAdapter(tracks : List<Track>?){
+    private fun createTopTracksAdapter(tracks: List<Track>?) {
         val topTracksAdapter = TopTracksAdapter(tracks, imageLoader)
         topRecyclerView.layoutManager = LinearLayoutManager(this.requireContext(), RecyclerView.VERTICAL, false)
         topRecyclerView.setHasFixedSize(true)
